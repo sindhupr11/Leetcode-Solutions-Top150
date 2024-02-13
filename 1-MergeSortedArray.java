9c4a96b547cdd40ed345183e82bd2ec83e2769d8
Sindhu P R <96954675+sindhupr11@users.noreply.github.com>
2024-02-13 07:03:39 +0530

    Update 1-MergeSortedArray.java

--- 1-MergeSortedArray.java
+++ 1-MergeSortedArray.java
@@ -6,6 +6,8 @@ Merge nums1 and nums2 into a single array sorted in non-decreasing order.
 
 The final sorted array should not be returned by the function, but instead be stored inside the array nums1. To accommodate this, nums1 has a length of m + n, where the first m elements denote the elements that should be merged, and the last n elements are set to 0 and should be ignored. nums2 has a length of n.
 
+https://leetcode.com/problems/merge-sorted-array/description/?envType=study-plan-v2&envId=top-interview-150
+
 */
 
 class Solution {
